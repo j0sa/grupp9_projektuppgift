61b9713e0f49d9ae7501f3ebff6f0d880fc30a2c
jomill99 <62940234+jomill99@users.noreply.github.com>
2020-12-18 16:18:26 +0100

    Update

--- DatingHemsida_Grupp_9/Models/User.cs
+++ DatingHemsida_Grupp_9/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,14 @@ namespace DatingHemsida_Grupp_9.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required (ErrorMessage = "Required field")]
         public String Namn { get; set; }
+
+       [Display(Name ="Detta är ett email fällt")]
+        public String Email { get; set; }
+
     }
 }
--- DatingHemsida_Grupp_9/wwwroot/js/site.js
+++ DatingHemsida_Grupp_9/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+            //alert("Test");
+
+//function Alert() {
+//    document.getElementById("Namn").nodeValue
+//    Alert("Test");
+
+//}
