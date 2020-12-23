e5ab2217e2d6d8fc8d44f88491a7b634f3d6d643
Johan Birgersson <johbi109@student.liu.se>
2020-12-23 10:35:45 +0100

    Test3

---
+++ DatingHemsida_Grupp_9/Create dbo.Profiles.sql
@@ -0,0 +1,20 @@
+﻿USE [DatingDb]
+GO
+
+/****** Object: Table [dbo].[Profiles] Script Date: 2020-12-23 09:56:37 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Profiles] (
+    [Id]          INT             IDENTITY (1, 1) NOT NULL,
+    [Firstname]   NVARCHAR (MAX)  NULL,
+    [Lastname]    NVARCHAR (MAX)  NULL,
+    [Gender]      NVARCHAR (MAX)  NULL,
+    [UserPicture] VARBINARY (MAX) NULL
+);
+
+
+Insert into [dbo].[Profiles] values('Adam', 'Test', 'kvinna', 100)
