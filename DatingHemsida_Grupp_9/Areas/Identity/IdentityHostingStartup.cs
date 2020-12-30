c0ecc1e55ad81f70a6f6a21e7b1c8b930e9c1908
jomill99 <62940234+jomill99@users.noreply.github.com>
2020-12-30 11:18:24 +0100

    Save

---
+++ DatingHemsida_Grupp_9/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DatingHemsida_Grupp_9.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DatingHemsida_Grupp_9.Areas.Identity.IdentityHostingStartup))]
+namespace DatingHemsida_Grupp_9.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- DatingHemsida_Grupp_9/Startup.cs
+++ DatingHemsida_Grupp_9/Startup.cs
@@ -35,7 +35,7 @@ namespace DatingHemsida_Grupp_9
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
