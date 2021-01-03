a2aa3dced8c094716c4ac2a6a890ac3a58aed031
jomill99 <62940234+jomill99@users.noreply.github.com>
2021-01-03 17:20:41 +0100

    Wall
    Kommentarer

--- DatingHemsida_Grupp_9/Controllers/WallController.cs
+++ DatingHemsida_Grupp_9/Controllers/WallController.cs
@@ -22,119 +22,59 @@ namespace DatingHemsida_Grupp_9.Controllers
         public ActionResult Index(int? profileId)
         // GET: Wall
         {
-            
-            
-
+            //Hämtar användaren
             var UserName = User.Identity.Name;
             var user = _DatingContext.Profiles.SingleOrDefault(p => p.Email == UserName);
-            
+
+            //Om inloggad och egen profil skapad
             if (UserName != null && user != null)
-            { 
+            {
+                //Kollar efter nya vänförfrågningar
                 FriendRequestVisible();
 
-var messages = _DatingContext.Messages.ToList();
+                //Hämtar alla meddelanden
+                var messages = _DatingContext.Messages.ToList();
 
+                //Skapar en tom lista av meddelanden
                 var mot = new List<DataLayer.Models.Message>();
 
-                if (profileId!=null) {
-
+                //Om en parameterskickas med
+                if (profileId != null)
+                {
+                    //Fyller tomma listan baserat på int parametern
                     mot = messages.Where(x => x.ReciverId == profileId).ToList();
+
+                    //Uppdaterar user till den profil man tryckt på
                     user = _DatingContext.Profiles.SingleOrDefault(p => p.Id == profileId);
                 }
-                else {mot = messages.Where(x => x.ReciverId == user.Id).ToList(); }
-                
-                
-            
-
-            var profiles = _DatingContext.Profiles.ToList();
-
-            Profile profile = new Profile()
-            {
-                Id = user.Id,
-                Firstname = user.Firstname,
-                Lastname = user.Lastname,
-                Age = user.Age,
-                Email = user.Email,
-                Gender = user.Gender,
-                SexualOrientation = user.SexualOrientation,
-                ImagePath = user.ImagePath
-            };
-
-            List<Message> messages1 = new List<Message>()
-            {
-            };
-            foreach (var m in mot)
-            {
-                Message message = new Message()
+                //Annars om en parameter inte skickas med
+                else
                 {
-                    MessageId = m.MessageId,
-                    SenderId = m.SenderId,
-                    ReciverId = m.ReciverId,
-                    Text = m.Text,
-                    Date = m.Date,
-                    Author = profiles.Single(x => x.Id == m.SenderId).Firstname
-                };
-                messages1.Add(message);
-            };
+                    //Fyller tomma listan med egna meddelanden
+                    mot = messages.Where(x => x.ReciverId == user.Id).ToList();
+                }
 
-            WallViewModel wallViewModel = new WallViewModel()
-            {
-                Profile = profile,
-                WallMessages = messages1
-            };
-
-            return View(wallViewModel);
-            
-            }
-            //else if (UserName != null && user == null)
-            //{
-            //    return RedirectToAction("Create", "Profile");
-            //}
-            else { return RedirectToAction("Index", "Home"); }
-        }
-
-
-
-
-
-
-
-        [Authorize]
-        public ActionResult GoToProfile(int ProfileId)
-        // GET: Wall
-        {
-
-
-
-            var UserName = User.Identity.Name;
-            var user = _DatingContext.Profiles.SingleOrDefault(p => p.Email == UserName);
-
-            if (UserName != null && user != null)
-            {
-
-                FriendRequestVisible();
-
-                var messages = _DatingContext.Messages.ToList();
-                var mot = messages.Where(x => x.ReciverId == ProfileId).ToList();
-
-                var profiles = _DatingContext.Profiles.ToList();
-                var userr = _DatingContext.Profiles.SingleOrDefault(p => p.Id == ProfileId);
-
+                //Skapar ny profil för den som ska visas
                 Profile profile = new Profile()
                 {
-                    Id = userr.Id,
-                    Firstname = userr.Firstname,
-                    Lastname = userr.Lastname,
-                    Age = userr.Age,
-                    Email = userr.Email,
-                    Gender = userr.Gender,
-                    SexualOrientation = userr.SexualOrientation,
-                    ImagePath = userr.ImagePath
+                    Id = user.Id,
+                    Firstname = user.Firstname,
+                    Lastname = user.Lastname,
+                    Age = user.Age,
+                    Email = user.Email,
+                    Gender = user.Gender,
+                    SexualOrientation = user.SexualOrientation,
+                    ImagePath = user.ImagePath
                 };
 
-                List<Message> messages1 = new List<Message>()
-                {
-                };
+                //Hämtar alla profiler i databas
+                var profiles = _DatingContext.Profiles.ToList();
+
+                //Ny tom lista
+                List<Message> messages1 = new List<Message>();
+
+                //Fyller tomma listan med nya meddelanden från databasen och ger dem en author genom
+                //att jämföra SenderId med profilerna i profiles-listan
                 foreach (var m in mot)
                 {
                     Message message = new Message()
@@ -148,7 +88,7 @@ var messages = _DatingContext.Messages.ToList();
                     };
                     messages1.Add(message);
                 };
-
+                //Skapar en viewmodel och fyller den med profil och profilens meddelanden
                 WallViewModel wallViewModel = new WallViewModel()
                 {
                     Profile = profile,
@@ -156,38 +96,31 @@ var messages = _DatingContext.Messages.ToList();
                 };
 
                 return View(wallViewModel);
-
             }
-            
+            //Om ej inloggad och ej profil
             else { return RedirectToAction("Index", "Home"); }
         }
-
-
-
-
-
-
 
         //Kontrollerar navbar
         public void FriendRequestVisible()
         {
-            //Skickar true or false till vyn för att visa knappen friendrequests om det finns några
+            //Skickar true or false till vyn för att visa om det finns vänförfrågningar
             ViewBag.Requests = false;
             var user = User.Identity.Name;
 
             var profile = _DatingContext.Profiles.SingleOrDefault(p => p.Email.Equals(user));
-            
-                
-                var id = profile.Id;
 
-                var listatva = _DatingContext.FriendRequests.Where(x => x.FriendReciverId.Equals(id))
-                    .Where(x => x.Accepted == false).Select(x => x.FriendSenderId).ToList();
+            var id = profile.Id;
 
-                if (listatva.Count > 0)
-                {
-                    ViewBag.Requests = true;
-                }
+            //Lista av vänförfrågningar
+            var listatva = _DatingContext.FriendRequests.Where(x => x.FriendReciverId.Equals(id))
+                .Where(x => x.Accepted == false).Select(x => x.FriendSenderId).ToList();
             
+            //Om listan av vänförfrågningar är större än 0
+            if (listatva.Count > 0)
+            {
+                ViewBag.Requests = true;
+            }
         }
 
         // GET: Wall/Details/5
